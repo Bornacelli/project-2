1aa02570d537a074e3941a5a00073b0400427544
Bornacelli <149744438+Bornacelli@users.noreply.github.com>
2024-02-21 00:20:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// link de la barra
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    // desaparecer menu en responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+// menu 
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//barra de habilidades
+window.onscroll = function() { efectoHabilidades() };
+
+//animación de la barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+
+}
